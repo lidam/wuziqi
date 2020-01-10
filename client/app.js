2f66af18e6ec360ca4716e717d73653c598c4690
UXIN\lidan9 <413334136@qq.com>
2020-01-10 18:08:06 +0800

    feat：代码优化，将画棋盘、画棋子、清空棋盘、判断输赢等提出放到一个类中

--- client/app.js
+++ client/app.js
@@ -1,33 +1,79 @@
 var chessGame = (function() {
   var canvas = document.getElementById('tutorial');
   var button = document.getElementById('HuiQi');
-  var ctx = canvas.getContext('2d');
+  var startBtn = document.getElementById('startPlay');
+  var types = document.getElementsByName('chessType');
+
   let list = [];
   const chessUnitWidth = 30;
-  const chessUnitNum = 15;
-  canvas.width = chessUnitWidth*(chessUnitNum + 1);
-  canvas.height = canvas.width;
 
+  let ChessInstance = null;
+
+  // 获取选中的类型
+  function getSelectedType(types) {
+    for(let i=0,j=types.length;i<j;i++){
+      if(types[i].checked){
+        return types[i].value
+      }
+    }
+    return undefined;
+  }
+  // 生成实例
+  const generateChessInstance = {
+    wuziqi() {
+      return new Wuziqi(canvas, chessUnitWidth);
+    },
+    siziqi() {
+      return new Siziqi(canvas, chessUnitWidth);
+    },
+    weiqi() {
+      return new Weiqi(canvas, chessUnitWidth);
+    }
+  }
+  // 绑定事件
+  function bindEvents() {
+    startBtn.addEventListener('click', function() {
+      const type = getSelectedType(types)
+      ChessInstance = generateChessInstance[type]();
+      ChessInstance.drawChessBoard();
+    });
+    // 下棋
+    canvas.addEventListener('click', play); 
+    // 悔棋
+    button.addEventListener('click', HuiQi)
+  }
+  // 下棋
+  function play(e) {
+    const chess = {
+      x: Math.round(e.clientX/ChessInstance.getUnitWidth()) * ChessInstance.getUnitWidth(),
+      y: Math.round(e.clientY/ChessInstance.getUnitWidth()) * ChessInstance.getUnitWidth(),
+      color: list.length % 2 === 0 ? 'black' : 'white'
+    }
+    if(!ChessInstance.isValidChessPosition(e, chess, list)) {
+      return;
+    }
+    list.push(chess);
+    // 棋子同步
+    window.dispatchEvent(new CustomEvent('updateChessList', {detail: list}));
+    // 判断是否赢了
+    if(ChessInstance.checkWin(chess, list)){
+      console.log('赢了');
+    }
+  }
+  // 悔棋
+  function HuiQi() {
+    // 删除当前的最后一步棋
+    list.pop();
+    // 棋子同步
+    window.dispatchEvent(new CustomEvent('updateChessList', {detail: list}));
+  }
   // 画棋盘
   function drawChessBoard() {
-    for(let i=1;i<chessUnitNum+1;i++){
-      ctx.beginPath();
-      ctx.moveTo(chessUnitWidth, i*chessUnitWidth);
-      ctx.lineTo(chessUnitWidth*chessUnitNum, i*chessUnitWidth);
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.moveTo(i*chessUnitWidth, chessUnitWidth);
-      ctx.lineTo(i*chessUnitWidth, chessUnitWidth*chessUnitNum);
-      ctx.stroke();
-    }
+    ChessInstance.drawChessBoard();
   }
   // 绘制棋子
   function drawChess(chess) {
-    ctx.beginPath();
-    ctx.arc(chess.x, chess.y, 10, 0, Math.PI*2, true);
-    ctx.fillStyle = chess.color;
-    ctx.fill();
+    ChessInstance.drawChess(chess);
   }
   // 获取棋子
   function getChessList() {
@@ -39,70 +85,8 @@ var chessGame = (function() {
   }
   // 清空棋盘
   function clearCanvas() {
-    // 清空棋盘
-    ctx.clearRect(chessUnitWidth/2, chessUnitWidth/2, chessUnitWidth*chessUnitNum + chessUnitWidth/2, chessUnitWidth*chessUnitNum + chessUnitWidth/2);
+    ChessInstance.clearChessBoard();
   }
-  // 是否越界
-  function isCrossTheBorder(e) {
-    const x = e.clientX;
-    const y = e.clientY;
-    if(x <= chessUnitWidth/2 ||
-       x >= chessUnitWidth*chessUnitNum + chessUnitWidth/2 ||
-       y <= chessUnitWidth/2 ||
-       y >= chessUnitWidth*chessUnitNum + chessUnitWidth/2){
-      return true;
-    }
-    return false;
-  }
-  // 是否是重复的位置
-  function isRepeatPosition(chessInfo) {
-    const repeatList = list.filter(item => {
-      return item.x === chessInfo.x && item.y === chessInfo.y;
-    })
-    return repeatList.length > 0
-  }
-  // 下棋
-  function play(e) {
-    if(isCrossTheBorder(e)){
-      return;
-    }
-    const chess = {
-      x: Math.round(e.clientX/chessUnitWidth) * chessUnitWidth,
-      y: Math.round(e.clientY/chessUnitWidth) * chessUnitWidth,
-      color: list.length % 2 === 0 ? 'black' : 'white'
-    }
-    if(isRepeatPosition(chess)){
-      return;
-    }
-    list.push(chess);
-    // 棋子同步
-    window.dispatchEvent(new CustomEvent('updateChessList', {detail: list}));
-    // 判断是否赢了
-    if(Chess.checkWin({
-      type: 'wuziqi', 
-      chessInfo: chess, 
-      chessUnitWidth, 
-      list
-    })){
-      console.log('赢了');
-    }
-  }
-  // 悔棋
-  function HuiQi() {
-    // 删除当前的最后一步棋
-    list.pop();
-    // 棋子同步
-    window.dispatchEvent(new CustomEvent('updateChessList', {detail: list}));
-  }
-  // 绑定事件
-  function bindEvents() {
-    // 下棋
-    canvas.addEventListener('click', play); 
-    // 悔棋
-    button.addEventListener('click', HuiQi)
-  }
-  // 初始化
-  drawChessBoard();
   bindEvents();
   return {
     drawChessBoard,
---
+++ client/chess/Chess.js
@@ -0,0 +1,167 @@
+class Chess {
+  constructor(canvas, unitWidth) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.unitWidth = unitWidth;
+  }
+  // 设置棋盘
+  _setChessBoard() {
+    this.canvas.width = this.unitWidth*(this.lineNum + 1);
+    this.canvas.height = this.canvas.width;
+  }
+  getUnitWidth() {
+    return this.unitWidth;
+  }
+  // 画棋盘
+  drawChessBoard() {
+    // 设置棋盘大小
+    this._setChessBoard();
+    // 绘制棋盘
+    for(let i=1;i<this.lineNum+1;i++){
+      // 横线
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.unitWidth, i*this.unitWidth);
+      this.ctx.lineTo(this.unitWidth*this.lineNum, i*this.unitWidth);
+      this.ctx.stroke();
+      // 纵线
+      this.ctx.beginPath();
+      this.ctx.moveTo(i*this.unitWidth, this.unitWidth);
+      this.ctx.lineTo(i*this.unitWidth, this.unitWidth*this.lineNum);
+      this.ctx.stroke();
+    }
+  }
+  // 画棋子
+  drawChess(chessInfo) {
+    this.ctx.beginPath();
+    this.ctx.arc(chessInfo.x, chessInfo.y, 10, 0, Math.PI*2, true);
+    this.ctx.fillStyle = chessInfo.color;
+    this.ctx.fill();
+  }
+  // 是否越界
+  _isCrossTheBorder(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    if(x <= this.unitWidth/2 ||
+       x >= this.unitWidth*this.lineNum + this.unitWidth/2 ||
+       y <= this.unitWidth/2 ||
+       y >= this.unitWidth*this.lineNum + this.unitWidth/2){
+      return true;
+    }
+    return false;
+  }
+  // 是否是重复的位置
+  _isRepeatPosition(chessInfo, list) {
+    const repeatList = list.filter(item => {
+      return item.x === chessInfo.x && item.y === chessInfo.y;
+    })
+    return repeatList.length > 0
+  }
+  // 是否是有效的位置
+  isValidChessPosition(e, chess, list) {
+    if(this._isCrossTheBorder(e)){
+      return false;
+    }
+    if(this._isRepeatPosition(chess, list)){
+      return false;
+    }
+    return true;
+  }
+  // 清空棋盘
+  clearChessBoard() {
+    this.ctx.clearRect(this.unitWidth/2, this.unitWidth/2, this.unitWidth*this.lineNum + this.unitWidth/2, this.unitWidth*this.lineNum + this.unitWidth/2);
+  }
+  // 检查是否赢棋
+  checkWin() {
+    throw new Error('请写具体实现');
+  }
+}
+// 五子棋
+class Wuziqi extends Chess {
+  constructor(ctx, unitWidth) {
+    super(ctx, unitWidth);
+    this.lineNum = 15;
+    this.blackNum = 113;
+    this.whiteNum = 112;
+  }
+  // 判断输赢
+  checkWin(chessInfo, list) {
+    let winCaseList = []
+    for(let k=0;k<4;k++){
+      winCaseList[k] = winCaseList[k] || [];
+      for(let i=0;i<5;i++){
+        winCaseList[k][i] = winCaseList[k][i] || [];
+        for(let j=-i;j<5-i;j++){
+          // 横着
+          if(k === 0){
+            winCaseList[k][i].push({
+              x: chessInfo.x - j * this.unitWidth,
+              y: chessInfo.y,
+              color: chessInfo.color
+            });
+          }
+          // 竖
+          if(k === 1){
+            winCaseList[k][i].push({
+              x: chessInfo.x,
+              y: chessInfo.y - j * this.unitWidth,
+              color: chessInfo.color
+            });
+          }
+          // 右斜
+          if(k === 2){
+            winCaseList[k][i].push({
+              x: chessInfo.x - j * this.unitWidth,
+              y: chessInfo.y + j * this.unitWidth,
+              color: chessInfo.color
+            });
+          }
+          // 左斜
+          if(k === 3){
+            winCaseList[k][i].push({
+              x: chessInfo.x - j * this.unitWidth,
+              y: chessInfo.y - j * this.unitWidth,
+              color: chessInfo.color
+            });
+          }
+        }
+      }
+    }
+    const thisColorList = list.filter(item => item.color === chessInfo.color);
+    var flag = winCaseList.some(items => {
+      return items.some(winList => {
+        return winList.every(item => {
+          return thisColorList.filter(chess => {
+            return item.x===chess.x && item.y===chess.y && item.color===chess.color
+          }).length > 0
+        })
+      })
+    });
+    return flag;
+  }
+}
+// 四子棋
+class Siziqi extends Chess {
+  constructor(ctx, unitWidth) {
+    super(ctx, unitWidth);
+    this.lineNum = 9;
+    this.blackNum = 41;
+    this.whiteNum = 40;
+  }
+  // 判断输赢
+  checkWin(chessInfo, list) {
+    console.log('正在努力建设中。。。')
+  }
+}
+// 围棋
+class Weiqi extends Chess {
+  constructor(ctx, unitWidth) {
+    super(ctx, unitWidth);
+    this.lineNum = 19;
+    this.blackNum = 181;
+    this.whiteNum = 180;
+  }
+  // 判断输赢
+  checkWin(chessInfo, list) {
+    console.log('正在努力建设中。。。')
+  }
+}
